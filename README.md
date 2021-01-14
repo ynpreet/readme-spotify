2decd222ad554df78bc9d4ebb496719d3607286c
ynpreet <preetmehta1995@gmail.com>
2021-01-14 12:29:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spotify GitHub Readme
+
+This is a small Vercel function to generate an image for your GitHub profile with your last playing song.
+
+## Deploy Your Own
+
+Deploy your own version
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/webmail/readme-spotify)
+
+# Adding environment variables
+
+Head to your project's settings on Vercel, go to "Environment Variables" and add `LASTFM_API_KEY` with your last.fm API key.
---
+++ api/generate.ts
@@ -0,0 +1,93 @@
+import { NowRequest, NowResponse } from "@now/node";
+import tti from "text-to-image";
+import fetch from "node-fetch";
+
+const { LASTFM_API_KEY } = process.env;
+
+const generate = async (username: string, darkTheme: boolean = false) => {
+  const endpoint = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${LASTFM_API_KEY}&format=json&limit=1`;
+  const request = await fetch(endpoint);
+
+  const body = (await request.json()) as APIResponse;
+  const tracks = body.recenttracks.track;
+  const lastTrack = tracks[0];
+
+  const content = `Last listening to ${lastTrack.name} by ${lastTrack.artist["#text"]}`;
+
+  return {
+    image: tti.generate(content, {
+      fontFamily: "sans-serif"
+    }),
+    content
+  };
+};
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const { image: body, content } = await generate(
+    req.query.username as string,
+    req.query.dark === "true"
+  );
+
+  const img = Buffer.from(body.split(",")[1], "base64");
+
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": img.length,
+    "X-Song": content
+  });
+
+  res.end(img);
+};
+
+export interface APIResponse {
+  recenttracks: Recenttracks;
+}
+
+export interface Recenttracks {
+  "@attr": Attr;
+  track: Track[];
+}
+
+export interface Attr {
+  page: string;
+  total: string;
+  user: string;
+  perPage: string;
+  totalPages: string;
+}
+
+export interface Track {
+  artist: Artist;
+  "@attr"?: Attr2;
+  mbid: string;
+  album: Album;
+  streamable: string;
+  url: string;
+  name: string;
+  image: Image[];
+  date?: Date;
+}
+
+export interface Artist {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Attr2 {
+  nowplaying: string;
+}
+
+export interface Album {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Image {
+  size: string;
+  "#text": string;
+}
+
+export interface Date {
+  uts: string;
+  "#text": string;
+}
